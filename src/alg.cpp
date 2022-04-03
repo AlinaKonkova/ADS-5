c950d00299068200e65e5dbeabbdc4cd182bdb5e
Alina Konkova <99170615+AlinaKonkova@users.noreply.github.com>
2022-04-03 18:01:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,8 @@
 #include <map>
 #include "tstack.h"
 
-int Prioritet(char prior) {
-  switch (prior) {
+int prioritet(char znak) {
+  switch (znak) {
   case '(': return 0;
   case ')': return 1;
   case '-': return 2;
@@ -16,43 +16,43 @@ int Prioritet(char prior) {
   }
 }
 
-int Calculator(char prior, int x, int y) {
-    switch (prior) {
-    case '-': return x - y;
-    case '+': return x + y;
-    case '*': return x * y;
-    case '/': 
-      if (x != 0)
-        return y / x;
-    default: return 0;
-    }
+int calculator(char znak, int x, int y) {
+  switch (znak) {
+  case '+': return (x + y);
+  case '-': return (y - x);
+  case '*': return (x * y);
+  case '/':
+    if (x != 0)
+      return y / x;
+  default: return 0;
+  }
 }
 
 std::string infx2pstfx(std::string inf) {
-  std::string result;
+  std::string res;
   char probel = ' ';
   TStack <char, 100> stack1;
   for (int i = 0; i < inf.size(); i++) {
-    if (Prioritet(inf[i]) == 4) {
+    if (prioritet(inf[i]) == 4) {
       result.push_back(inf[i]);
       result.push_back(probel);
     } else {
-      if (Prioritet(inf[i]) == 0) {
+      if (prioritet(inf[i]) == 0) {
         stack1.push(inf[i]);
       } else if (stack1.isEmpty()) {
           stack1.push(inf[i]);
-      } else if ((Prioritet(inf[i]) > Prioritet(stack1.get()))) {
+      } else if ((prioritet(inf[i]) > prioritet(stack.get()))) {
           stack1.push(inf[i]);
-      } else if (Prioritet(inf[i]) == 1) {
-        while (Prioritet(stack1.get()) != 0) {
+      } else if (prioritet(inf[i]) == 1) {
+        while (prioritet(stack1.get()) != 0) {
           result.push_back(stack1.get());
           result.push_back(probel);
           stack1.pop();
         }
         stack1.pop();
       } else {
-        char pr = Prioritet(inf[i]);
-        char pr2 = Prioritet(stack1.get());
+        char pr = prioritet(inf[i]);
+        char pr2 = prioritet(stack1.get());
         while ((pr <= pr2) && (!stack1.isEmpty())) {
           result.push_back(stack1.get());
           result.push_back(probel);
@@ -73,24 +73,23 @@ std::string infx2pstfx(std::string inf) {
   }
   return result;
 }
-
-
 int eval(std::string pref) {
   TStack <int, 100> stack2;
   int result = 0;
   int x = 0;
   int y = 0;
   for (int i = 0; i < pref.size(); i++) {
-    if (Prioritet(pref[i]) == 4) {
+    if (prioritet(pref[i]) == 4) {
       stack2.push(pref[i] - '0');
-    } else if (Prioritet(pref[i]) < 4) {
+    } else if (prioritet(pref[i]) < 4) {
       x = stack2.get();
       stack2.pop();
-      y = stack2.get();
+      y = res_stack.get();
       stack2.pop();
-      stack2.push(Calculator(pref[i], x, y));
+      stack2.push(calculator(pref[i], x, y));
     }
   }
   result = stack2.get();
   return result;
+  return 0;
 }
